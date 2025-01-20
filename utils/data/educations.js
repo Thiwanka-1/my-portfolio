dff69d4de798a37b0acb6426283a9d39e2dc0405
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2025-01-20 10:44:51 +0530

    add new education skills and projects

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,17 +2,23 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2024 - Present",
+    duration: "2022 - Present",
     institution: "Sri Lanka Institute of Information Technology",
   },
   {
     id: 2,
+    title: "Higher Diploma In Information Technology Certificate",
+    duration: "2022 - 2024",
+    institution: "Sri Lanka Institute of Information Technology",
+  },
+  {
+    id: 3,
     title: "Advanced Level Exam Certificate",
     duration: "2020 - 2021",
     institution: "Taxila Central College, Horana",
   },
   {
-    id: 3,
+    id: 4,
     title: "Ordinary Level Exam Certificate",
     duration: "2016 - 2018",
     institution: "Sri Sumangala College, Panadura",
